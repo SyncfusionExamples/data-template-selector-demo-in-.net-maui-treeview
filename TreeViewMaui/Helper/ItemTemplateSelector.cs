bea4052737f06452b0d19c91abc502fa01b7968d
DhivyaSF4094 <dhivya.sf4094@gmail.com>
2023-09-12 11:06:39 +0530

    Prepared sample for TemplateSelector.

---
+++ TreeViewMaui/Helper/ItemTemplateSelector.cs
@@ -0,0 +1,34 @@
+﻿using Syncfusion.TreeView.Engine;
+using TreeViewMaui.TemplateSelector;
+
+namespace TreeViewMaui
+{
+    internal class ItemTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate Template1 { get; set; }
+        public DataTemplate Template2 { get; set; }
+        public ItemTemplateSelector()
+        {
+            this.Template1 = new DataTemplate(typeof(Template1));
+            this.Template2 = new DataTemplate(typeof(Template2));
+        }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var treeNodeView = item as TreeViewNode;
+            if (treeNodeView == null)
+            {
+                return null;
+            }
+
+            if (treeNodeView.Level == 0)
+            {
+                return Template1;
+            }
+            else
+            {
+                return Template2;
+            }
+        }
+    }
+}
---
+++ TreeViewMaui/TemplateSelector/Template2.xaml.cs
@@ -0,0 +1,9 @@
+namespace TreeViewMaui.TemplateSelector;
+
+public partial class Template2 : ViewCell
+{
+	public Template2()
+	{
+		InitializeComponent();
+	}
+}
